6f8b6bac2be1a6970c667c9363d9946e0674bb56
Fragarias <148366640+Fragarias@users.noreply.github.com>
2023-12-04 15:12:06 +0000

    [Add] Bookモデルとcontroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # get 'homes/top'
+  devise_for :users
   root to: "homes#top"
   get "homes/about", as: 'about'
-  devise_for :users
-  # resources :books, only: [:index, :show]
+  resources :books, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit]
+  # get 'homes/top'
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/edit'
+  # get 'users/index'
+  # get 'users/show'
+  # get 'users/edit'
 end
