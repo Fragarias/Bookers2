e3646567671678872d85e9abd7336e892ce22994
Fragarias <148366640+Fragarias@users.noreply.github.com>
2023-12-09 14:00:06 +0000

    [Add] Bookseditビュー

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path(book.id)
+    redirect_to book_path(@book.id)
   end
 
   def show
@@ -20,7 +20,9 @@ class BooksController < ApplicationController
   end
 
   def update
-
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   def destroy
