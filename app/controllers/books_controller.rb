4ad2faa670abd21ff327ab0b5cecc1c225e28aa5
Fragarias <148366640+Fragarias@users.noreply.github.com>
2023-12-12 17:07:23 +0000

    [Delete] 不要なコード

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,7 +12,6 @@ class BooksController < ApplicationController
       flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
     else
-      @book_new = Book.new
       @books = Book.all
       render :index
     end
